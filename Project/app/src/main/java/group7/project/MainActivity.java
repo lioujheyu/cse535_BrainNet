4ee36ade1cb990b14100979e2430afd6dc3f54ba
yuhsuanchuang <ychuang8@asu.edu>
2017-11-29 00:50:08 -0700

    App : not complete
    -main.java
    fog_serverURL
    
    -server.java
    downloadFileName = ""
    
    -show testing result

---
+++ Project/app/src/main/java/group7/project/MainActivity.java
@@ -0,0 +1,311 @@
+package group7.project;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.CheckBox;
+import android.os.Handler;
+import android.widget.TextView;
+import android.widget.BaseAdapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.app.Activity;
+import android.widget.ListView;
+import android.content.res.TypedArray;
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.AdapterView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public class Item {
+        boolean checked;
+        String ItemString;
+        Item( String t, boolean b){
+            ItemString = t;
+            checked = b;
+        }
+
+        public boolean isChecked(){
+            return checked;
+        }
+    }
+
+    static class ViewHolder {
+        CheckBox checkBox;
+        TextView text;
+    }
+
+    public class ItemsListAdapter extends BaseAdapter {
+
+        private Context context;
+        private List<Item> list;
+
+        ItemsListAdapter(Context c, List<Item> l) {
+            context = c;
+            list = l;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public boolean isChecked(int position) {
+            return list.get(position).checked;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View rowView = convertView;
+
+            // reuse views
+            ViewHolder viewHolder = new ViewHolder();
+            if (rowView == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                rowView = inflater.inflate(R.layout.row, null);
+
+                viewHolder.checkBox = (CheckBox) rowView.findViewById(R.id.rowCheckBox);
+                viewHolder.text = (TextView) rowView.findViewById(R.id.rowTextView);
+                rowView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) rowView.getTag();
+            }
+
+            viewHolder.checkBox.setChecked(list.get(position).checked);
+
+            final String itemStr = list.get(position).ItemString;
+            viewHolder.text.setText(itemStr);
+
+            viewHolder.checkBox.setTag(position);
+            viewHolder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    boolean newState = !list.get(position).isChecked();
+                    list.get(position).checked = newState;
+                }
+            });
+
+            viewHolder.checkBox.setChecked(isChecked(position));
+
+            return rowView;
+        }
+    }
+    List<Item> items;
+    ListView listView;
+    ItemsListAdapter myItemsListAdapter;
+
+
+    public Toast mToast;
+    private Button registerbutton, loginbutton;
+    private RadioGroup radioGroup;
+    private String serverchoose;
+
+    String db_path = Environment.getExternalStorageDirectory() + "/Android/Data/PROJECT_DATA";
+    String remote_serverURL = "https://www.lioujheyu.com";
+    String fog_serverURL = "";
+    String serverURL = remote_serverURL;
+
+    String train_serverPHPfile = "train_UploadToServer.php";
+    String test_serverPHPfile = "test_UploadToServer.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        jump_to_page_1();
+    }
+
+    private void initItems(){
+        items = new ArrayList<Item>();
+
+        TypedArray arrayText = getResources().obtainTypedArray(R.array.restext);
+
+        for(int i = 0; i < arrayText.length(); i++){
+            String s = arrayText.getString(i);
+            boolean b = false;
+            Item item = new Item(s, b);
+            items.add(item);
+        }
+
+        arrayText.recycle();
+    }
+
+
+    public void jump_to_register_server(){
+        setContentView(R.layout.upload_layout);
+        listView = (ListView)findViewById(R.id.listview);
+
+        initItems();
+        myItemsListAdapter = new ItemsListAdapter(this, items);
+        listView.setAdapter(myItemsListAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        ((Item)(parent.getItemAtPosition(position))).ItemString,
+                        Toast.LENGTH_SHORT).show();
+            }});
+
+        Button uploadbtn= (Button)findViewById(R.id.button3);
+        Button jumpbackbtn= (Button)findViewById(R.id.button4);
+
+        uploadbtn.setOnClickListener(new Button.OnClickListener() {
+            List<String> CheckBoxlist = new ArrayList<>();
+            String[] CheckBoxarray;
+            @Override
+            public void onClick(View arg0) {
+                for (int i=0; i<items.size(); i++){
+                    if (items.get(i).isChecked()){
+                        CheckBoxlist.add(Integer.toString(i+1));
+                    }
+                }
+
+                CheckBoxarray = CheckBoxlist.toArray(new String[CheckBoxlist.size()]);
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        serverConnection conn = new serverConnection(serverURL, train_serverPHPfile, MainActivity.this);
+                        conn.uploadFile(db_path, CheckBoxarray, true);  // true => register
+                    }
+                }).start();
+                CheckBoxlist.clear();
+            }
+        });
+        jumpbackbtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                jump_to_page_1();
+            }
+        });
+
+    }
+
+    public void jump_to_login_server(){
+        setContentView(R.layout.test_layout);
+        listView = (ListView)findViewById(R.id.listview);
+
+        initItems();
+        myItemsListAdapter = new ItemsListAdapter(this, items);
+        listView.setAdapter(myItemsListAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        ((Item)(parent.getItemAtPosition(position))).ItemString,
+                        Toast.LENGTH_SHORT).show();
+            }});
+
+        Button uploadbtn= (Button)findViewById(R.id.button3);
+        Button jumpbackbtn= (Button)findViewById(R.id.button4);
+
+        uploadbtn.setOnClickListener(new Button.OnClickListener() {
+            List<String> CheckBoxlist = new ArrayList<>();
+            String[] CheckBoxarray;
+            @Override
+            public void onClick(View arg0) {
+                for (int i=0; i<items.size(); i++){
+                    if (items.get(i).isChecked()){
+                        CheckBoxlist.add(Integer.toString(i+1));
+                    }
+                }
+                CheckBoxarray = CheckBoxlist.toArray(new String[CheckBoxlist.size()]);
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        serverConnection conn = new serverConnection(serverURL, test_serverPHPfile, MainActivity.this);
+                        conn.uploadFile(db_path, CheckBoxarray, false); // false => login
+                    }
+                }).start();
+                CheckBoxlist.clear();
+            }
+        });
+        jumpbackbtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                jump_to_page_1();
+            }
+        });
+
+    }
+
+    public void jump_to_page_1() {
+        setContentView(R.layout.activity_main);
+
+
+        registerbutton = (Button) findViewById(R.id.button);
+        loginbutton = (Button) findViewById(R.id.button2);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.check(R.id.radioButton2);
+        serverchoose = ((RadioButton) findViewById(radioGroup.getCheckedRadioButtonId())).getText().toString();
+
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serverchoose = ((RadioButton) findViewById(radioGroup.getCheckedRadioButtonId())).getText().toString();
+                if(serverchoose == "Remote server")
+                    serverURL = remote_serverURL;
+                else if(serverchoose == "Fog server")
+                    serverURL = fog_serverURL;
+
+                mToast = Toast.makeText(MainActivity.this, serverchoose, Toast.LENGTH_SHORT);
+                mToast.show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mToast.cancel();
+                    }
+                }, 500);
+                jump_to_register_server();
+            }
+        });
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serverchoose = ((RadioButton) findViewById(radioGroup.getCheckedRadioButtonId())).getText().toString();
+                if(serverchoose == "Remote server")
+                    serverURL = remote_serverURL;
+                else if(serverchoose == "Fog server")
+                    serverURL = fog_serverURL;
+
+                mToast = Toast.makeText(MainActivity.this, serverchoose, Toast.LENGTH_SHORT);
+                mToast.show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mToast.cancel();
+                    }
+                }, 500);
+                jump_to_login_server();
+            }
+        });
+    }
+
+}
---
+++ Project/app/src/main/java/group7/project/serverConnection.java
@@ -0,0 +1,224 @@
+package group7.project;
+
+import android.os.Handler;
+import android.util.Log;
+import android.view.ViewDebug;
+import android.widget.Toast;
+import android.text.TextUtils;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.channels.FileLock;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
+
+// This class is referred to http://tinyurl.com/or8wql2
+class serverConnection {
+	private String serverURL; // should not include any filename
+    private String serverPHPfile; // Upload procedure will use this server-side program
+    private MainActivity main;
+
+	serverConnection(String serverURL, String serverPHPfile, MainActivity mainActivity) {
+		this.serverURL = serverURL;
+        this.serverPHPfile = serverPHPfile;
+		this.main = mainActivity;
+	}
+
+	void uploadFile(String uploadFilePath, String[] uploadFileName, boolean register_or_login) {
+        HttpsURLConnection conn;
+		DataOutputStream dos;
+		String lineEnd = "\r\n";
+		String twoHyphens = "--";
+		String boundary = "*****";
+		String filename = "", downloadFileName = "ttttest", downloadFilePath = uploadFilePath;
+		int bytesRead, bytesAvailable, bufferSize;
+		byte[] buffer;
+		int maxBufferSize = 1024*1024;
+		File uploadFile;
+		int start, end;
+		if(register_or_login == true){
+			start = 1;
+			end = 13;
+		}
+		else
+		{
+			start = 14;
+			end = 14;
+		}
+
+		try {
+			main.runOnUiThread(new Runnable() {
+				public void run() {
+					final Toast mToast = Toast.makeText(main, " Uploading...", Toast.LENGTH_SHORT);
+					mToast.show();
+					Handler handler = new Handler();
+					handler.postDelayed(new Runnable() {
+						@Override
+						public void run() {
+							mToast.cancel();
+						}
+					}, 500);
+				}
+			});
+			for(int i = 0; i <= uploadFileName.length; i++) {
+				for(int j = start; j <= end; j++) {
+					String response = "";
+					final int x = i*(end-start+1)+(j-start+1), l = uploadFileName.length * (end-start+1);
+
+					if (i == uploadFileName.length)
+						filename = "end";
+					else {
+						filename = "S"
+								+ new String(new char[3 - uploadFileName[i].length()]).replace("\0", "0")
+								+ uploadFileName[i] + "R"
+								+ new String(new char[2 - Integer.toString(j).length()]).replace("\0", "0")
+								+ Integer.toString(j) + ".edf";
+					}
+
+					uploadFile = new File(uploadFilePath + "/" + filename);
+
+					if (!uploadFile.exists())
+						Log.e("file error", "file not exist = " + filename);
+					else
+						Log.e("file success", "file exist = " + filename);
+
+					FileInputStream fileInputStream;
+					URL url = new URL(serverURL + "/" + serverPHPfile);
+
+					// Open a HTTP  connection to  the URL
+					conn = (HttpsURLConnection) url.openConnection();
+					conn.setDoInput(true); // Allow Inputs
+					conn.setDoOutput(true); // Allow Outputs
+					conn.setUseCaches(false); // Don't use a Cached Copy
+					conn.setRequestMethod("POST");
+					conn.setRequestProperty("Connection", "Keep-Alive");
+					conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+					conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+					// open a URL connection to the Server
+					fileInputStream = new FileInputStream(uploadFile);
+					conn.setRequestProperty("uploaded_file", filename);
+					dos = new DataOutputStream(conn.getOutputStream());
+					dos.writeBytes(twoHyphens + boundary + lineEnd);
+					dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + filename + "\"" + lineEnd);
+					dos.writeBytes(lineEnd);
+
+					// create a buffer of  maximum size
+					bytesAvailable = fileInputStream.available();
+					bufferSize = Math.min(bytesAvailable, maxBufferSize);
+					buffer = new byte[bufferSize];
+
+					// read file and write it into form...
+					bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+					while (bytesRead > 0) {
+						dos.write(buffer, 0, bufferSize);
+						bytesAvailable = fileInputStream.available();
+						bufferSize = Math.min(bytesAvailable, maxBufferSize);
+						bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+					}
+
+					// send multipart form data necessary after file data...
+					dos.writeBytes(lineEnd);
+					dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+
+					Log.e("get response", conn.getResponseMessage());
+					if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+						Log.e("upload success", "upload success = " + filename);
+						String line;
+						BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+						while ((line = br.readLine()) != null) {
+							response += line;
+						}
+						if (i < uploadFileName.length) {
+							main.runOnUiThread(new Runnable() {
+								public void run() {
+									final Toast mToast = Toast.makeText(main, Integer.toString(x) + "/" + Integer.toString(l) + " Uploaded.", Toast.LENGTH_SHORT);
+									mToast.show();
+									Handler handler = new Handler();
+									handler.postDelayed(new Runnable() {
+										@Override
+										public void run() {
+											mToast.cancel();
+										}
+									}, 500);
+								}
+							});
+						}
+					}
+
+					fileInputStream.close();
+					dos.flush();
+					dos.close();
+					conn.disconnect();
+					if (response.equals("end") && register_or_login == true) {
+						main.runOnUiThread(new Runnable() {
+							public void run() {
+								final Toast mToast = Toast.makeText(main, "Training Completed.", Toast.LENGTH_SHORT);
+								mToast.show();
+							}
+						});
+						break;
+					}
+					if(response.equals("end") && register_or_login == false)
+					{
+						try {
+							url = new URL(serverURL + "/" + downloadFileName);
+							conn = (HttpsURLConnection) url.openConnection();
+							//int responseCode = conn.getResponseCode();
+
+							File localPath = new File(downloadFilePath);
+							if (!localPath.exists())
+								localPath.mkdirs();
+
+							FileOutputStream outputStream =
+									new FileOutputStream(downloadFilePath + "/" + downloadFileName);
+							// Lock the file in case it is also used by the database
+							FileLock filelock = outputStream.getChannel().lock();
+
+							InputStream is = conn.getInputStream();
+							buffer = new byte[4096];
+							int current;
+							while ((current = is.read(buffer)) != -1) {
+								outputStream.write(buffer, 0, current);
+							}
+
+							filelock.release();
+							outputStream.close();
+							conn.disconnect();
+							main.runOnUiThread(new Runnable() {
+								public void run() {
+									final Toast mToast = Toast.makeText(main, "Download Completed", Toast.LENGTH_SHORT);
+									mToast.show();
+								}
+							});
+						}
+						catch (Exception e) {
+							final String errMsg = e.toString();
+							main.runOnUiThread(new Runnable() {
+								public void run() {
+									final Toast mToast = Toast.makeText(main, "Download Failed", Toast.LENGTH_SHORT);
+									mToast.show();
+								}
+							});
+						}
+						break;
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			Log.e("connect error", "connect error" + e);
+			main.runOnUiThread(new Runnable() {
+				public void run() {
+					Toast.makeText(main, "Upload Failed.", Toast.LENGTH_SHORT).show();
+				}
+			});
+		}
+	}
+}
